a8216f0c6fbd30d645c1ef81555744e18de2dba1
Andrew Badolato <21231601+andrewbadolato@users.noreply.github.com>
2018-05-02 21:35:51 -0400

    Add project files.

---
+++ RazorPagesMovie/Models/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Schedule> Schedule { get; set; }
+
+    }
+}
---
+++ RazorPagesMovie/Pages/Logout.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Data;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            return RedirectToPage("/Index");
+        }
+    }
+}
